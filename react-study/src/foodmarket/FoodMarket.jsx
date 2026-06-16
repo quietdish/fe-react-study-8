95b088134a9de3138b04e35947c73cffc9560111
quietdish <whale61@naver.com>
2026-06-16 16:27:25 +0900

    리액트 이미지 사용, bootstrap 컴포넌트 사용

--- react-study/src/foodmarket/FoodMarket.jsx
+++ react-study/src/foodmarket/FoodMarket.jsx
@@ -1,10 +1,135 @@
 import './FoodMarket.css';
+// import Button from 'react-bootstrap/Button';
+// import Spinner from 'react-bootstrap/Spinner';
+
+// import {} from 'react-bootstrap';
+import { Button, Spinner, Container, Nav, Navbar, Row, Col, Card } from 'react-bootstrap';
+import banner_bg from './img/banner_bg.jpg';
+import food1 from './img/food1.jpg';
+import food2 from './img/food2.jpg';
+import food3 from './img/food3.jpg';
+
 
 function FoodMarket() {
     return (
         <div>
 
-        </div>
+            <Navbar bg="light" data-bs-theme="light">
+                <Container>
+                    <Navbar.Brand href="#home">FoodMarket</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+
+            {/* 
+                이미지 사용
+                react 컴포넌트에서 이미지 사용시, import -> 사용
+                
+                <img src="https://....외부url"/>
+                //<img src="../../image/menu.png"/> XXX
+                <img src="{import한 이미지}/>
+            
+            */}
+
+            {/* <img src={banner_bg} /> */}
+            {/* <div className='main-bg' style={{ backgroundImage: 'url(' + banner_bg + ')'}}></div> */}
+            <div className='main-bg'></div>
+
+
+
+
+            {/* 
+                bootstrap 한줄 기준 : 12 
+            */}
+            <Container>
+                <Row>
+                    <Col sm={2} md={4}>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src={food1} />
+                            <Card.Body>
+                                <Card.Title>카드 타이틀 div태그</Card.Title>
+                                <Card.Text>
+                                    내부 내용쓰는 곳. p 태그 인식
+                                </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col sm={2} md={4}>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src={food2} />
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col sm={2} md={4}>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src={food3} />
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+
+
+
+
+                {/* <Col>1 of 2</Col>
+                    <Col>2 of 2</Col>
+                </Row>
+                <Row xs={1} md={2}>
+                    <Col>1 of 3</Col>
+                    <Col>2 of 3</Col>
+                    <Col>3 of 3</Col>
+                </Row>
+                <Row xs="auto">
+                    <Col>1 of 3</Col>
+                    <Col>2 of 3</Col>
+                    <Col>3 of 3</Col> */}
+
+            </Container>
+
+
+
+
+
+
+
+
+
+
+
+
+            {/* <Spinner animation="border" role="status">
+                <span className="visually-hidden">Loading...</span>
+            </Spinner>
+
+            <Button variant="success">Success</Button>
+            <Button variant="primary">Primary</Button>
+            <Button variant="secondary">Secondary</Button>
+            <Button variant="warning">Warning</Button>
+            <Button variant="danger">Danger</Button>
+            <Button variant="info">Info</Button>
+            <Button variant="light">Light</Button>
+            <Button variant="dark">Dark</Button>
+            <Button variant="link">Link</Button> */}
+        </div >
     );
 }
 

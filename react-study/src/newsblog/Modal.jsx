3fd5b6c6fd8e787c245df35c679594e39c7bb4c7
quietdish <whale61@naver.com>
2026-06-16 12:14:07 +0900

    모달창(컴포넌트)에 각종 props 전달 및 사용 방식

--- react-study/src/newsblog/Modal.jsx
+++ react-study/src/newsblog/Modal.jsx
@@ -1,11 +1,18 @@
 
 
-function Modal(){
-    return(
-        <div className="modal">
-            <h3>제목</h3>
+function Modal(props) {    //
+    return (
+        <div className="modal" style={{ backgroundColor: props.gbColor }}>
+            <h3>{props.title}❤{props.likeCount}</h3>
             <p>날짜</p>
-            <p>상세내용</p>
+            <p>상세내용</p> 
+            <button onClick={() => {
+                props.title = '긴급 뉴스';
+                let temp = [...props.news];
+                temp[1] = '긴급뉴스';
+                props.setNews(temp);
+
+            }}>제목 긴급 변경</button>
         </div>
     )
 }

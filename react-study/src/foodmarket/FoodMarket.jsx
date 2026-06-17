08ef6961ce5dc17bf5c3fc166bb989c78c31b414
quietdish <whale61@naver.com>
2026-06-17 15:13:27 +0900

    경로별 페이지처리 (react-router) 설치 및 설정 적용 페이지 단위 폴더 분할

--- react-study/src/foodmarket/FoodMarket.jsx
+++ react-study/src/foodmarket/FoodMarket.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 
 import './FoodMarket.css';
-import FoodCard from './components/FoodCard';
 
 // import Button from 'react-bootstrap/Button';
 // import Spinner from 'react-bootstrap/Spinner';
 
 // import {} from 'react-bootstrap';
 import { Button, Spinner, Container, Nav, Navbar, Row, Col, Card } from 'react-bootstrap';
+import FoodCard from './components/FoodCard';       //// 17일 14시 22분.........
+
+
 // import banner_bg from './img/banner_bg.jpg';
 // import food1 from './img/food1.jpg';
 // import food2 from './img/food2.jpg';
@@ -16,15 +18,48 @@ import { Button, Spinner, Container, Nav, Navbar, Row, Col, Card } from 'react-b
 // 화면에 보여주는  푸드 정보 기준 data import
 import foodsData from './data/foodsData';
 
+import { Routes, Route, Link, useNavigate } from 'react-router';    //// 17일 14시 03분....
+import CustomerService from './pages/CustomerService';
+import Home from './pages/Home';  // ← 추가
+
 
 function FoodMarket() {
 
     let [foods, setFoods] = useState(foodsData);
+    let navigate = useNavigate();
 
     return (
         <div>
-
             <Navbar bg="light" data-bs-theme="light">
+                <Container>
+                    <Navbar.Brand onClick={() => { navigate("/") }}>FoodMarket</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link onClick={() => { navigate("/") }}>Home</Nav.Link>
+                        <Nav.Link onClick={() => { navigate("/detail") }}>FoodDetail</Nav.Link>
+                        <Nav.Link onClick={() => { navigate("/info") }}>Info</Nav.Link>
+                        <Nav.Link onClick={() => { navigate("/help") }}>고객센터</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+
+            <Routes>
+                <Route path="/" element={<Home foods={foods} />} />
+                <Route path="/help" element={<CustomerService />} />
+                <Route path="/detail" element={<div><h1>detail page</h1></div>} />
+                <Route path="/info" element={<div><h1>info page</h1></div>} />
+                <Route path="/*" element={<div><h1>잘못된 접근입니다.</h1></div>} />
+            </Routes>
+        </div>
+    );
+}
+
+export default FoodMarket;
+
+
+
+
+
+            {/* <Navbar bg="light" data-bs-theme="light">
                 <Container>
                     <Navbar.Brand href="#home">FoodMarket</Navbar.Brand>
                     <Nav className="me-auto">
@@ -33,128 +68,49 @@ function FoodMarket() {
                         <Nav.Link href="#pricing">Pricing</Nav.Link>
                     </Nav>
                 </Container>
-            </Navbar>
+            </Navbar> */}
 
-            {/* 
-                이미지 사용
-
-                1. css 에서 사용
-                    이미지 경로 (상대경로 ./img/...)
-
-                2. js(jsx) 에서 사용
-
-                상대경로 연결사용 XXX
-                //
-
-                react 컴포넌트에서 이미지 사용시, import -> 사용
-                
-                <img src="https://....외부url"/>
-                //<img src="../../image/menu.png"/> XXX
-                <img src="{import한 이미지}/>
-
-                3. public 폴더에 이미지를 미리 저장해놓고 사용
-                현재 실행중인 서버가 public 경로를 통해 image 에 접근할 수 있는 서버 역할
-                이미지 외부 url에 접근해서 사용하듯이...사용
-
-                <img src={'http://localhost:5173/images/food1.jpg'}/>
-                <img src={'localhost:5173/images/food1.jpg'}/>
-                //<img src={'생략가능/images/food1.jpg'}/>
-                        <img src={'/images/food1.jpg'}/>
-
-                <img src={'http://localhost:7173/images/food1.jpg'}/>
-                <img src={'http://hellofoodmarket.com/images/food1.jpg'}/>
-
-                CRA     :   <img src={ process.env.PUBLIC_URL   + '/images/food1.jpg')/>
-                            package.json
-                            "homepage":"/detailServicePath" 상세경로설정
-                Vite    :   <ing src={ import.meta.env.BASE_URL   _ '/images/food1.jpg'}/>
-                            vite.config.js
-                            base: "/detailServicePath"  상세경로설정
-
-            
-            */}
 
 
 
             {/* <img src={banner_bg} /> */}
             {/* <div className='main-bg' style={{ backgroundImage: 'url(' + banner_bg + ')'}}></div> */}
-            <div className='main-bg'></div>
+            // <div className='main-bg'></div>
 
-
-            {/* 
-                bootstrap 한줄 기준 : 12 
-            */}
-            <Container>
-                <Row>
-                    {/* {
-                        foods.map(() => {
-                            <Col md={4} sm={2}>
-                                <FoodCard />
-                            </Col>
-                        })
-                    } */}
-
-
-
-                    {
-                        foods.map((food, index) => {
-                            return (
-                                <FoodCard
-                                    key={food.id}
-                                    food={food}
-                                    foods={foods}
-                                    index={index}
-                                />
-                            );
-                        })
-                    }
-
-                </Row>
-            </Container>
-
-            {/*
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src={food1} />
-                    <Card.Body>
-                        {/* <Card.Title>카드 타이틀 div태그</Card.Title>
-            <Card.Title>{foods[0].title}</Card.Title>
-            <Card.Text>{foods[0].content}{foods[0].price}</Card.Text>
-            <Button variant="primary">상세보기</Button>
-        </Card.Body>
-                </Card >                 
-            */
-            }
-
-            {/* <Col>1 of 2</Col>
-                    <Col>2 of 2</Col>
-                </Row>
-                <Row xs={1} md={2}>
-                    <Col>1 of 3</Col>
-                    <Col>2 of 3</Col>
-                    <Col>3 of 3</Col>
-                </Row>
-                <Row xs="auto">
-                    <Col>1 of 3</Col>
-                    <Col>2 of 3</Col>
-                    <Col>3 of 3</Col> */}
+            
+//                 bootstrap 한줄 기준 : 12 
+//             */}
+//             <Container>
+//                 <Row>
+//                     {
+//                         foods.map(() => {
+//                             <Col md={4} sm={2}>
+//                                 <FoodCard />
+//                             </Col>
+//                         })
+//                     }
 
 
 
-            {/* <Spinner animation="border" role="status">
-                <span className="visually-hidden">Loading...</span>
-            </Spinner>
+//                     {
+//                         foods.map((food, index) => {
+//                             return (
+//                                 <FoodCard
+//                                     key={food.id}
+//                                     food={food}
+//                                     foods={foods}
+//                                     index={index}
+//                                 />
+//                             );
+//                         })
+//                     }
 
-            <Button variant="success">Success</Button>
-            <Button variant="primary">Primary</Button>
-            <Button variant="secondary">Secondary</Button>
-            <Button variant="warning">Warning</Button>
-            <Button variant="danger">Danger</Button>
-            <Button variant="info">Info</Button>
-            <Button variant="light">Light</Button>
-            <Button variant="dark">Dark</Button>
-            <Button variant="link">Link</Button> */}
-        </div >
-    );
-}
+//                 </Row>
+//             </Container>
 
-export default FoodMarket;
+
+//         </div >
+//     );
+// }
+
+// export default FoodMarket;

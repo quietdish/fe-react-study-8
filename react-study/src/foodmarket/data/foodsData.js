d008406c528fc18a6ee0fc6a3dbc9f6a98f8aa7b
quietdish <whale61@naver.com>
2026-06-18 16:53:50 +0900

    foodsData 내용추가

--- react-study/src/foodmarket/data/foodsData.js
+++ react-study/src/foodmarket/data/foodsData.js
@@ -4,8 +4,8 @@ let foodsData = [
         title: "Cake",
         content: "당보충 초코 케이크",
         price: 8000,
-        stockCount: 10,
-        imgPath: 'images/food1.jpg'
+        imgPath: 'images/food1.jpg',
+        stockCount: 10
     },
 
     {
@@ -13,8 +13,8 @@ let foodsData = [
         title: "French Fries",
         content: "정신에 좋은 감자튀김",
         price: 9000,
-        stockCount: 5,
-        imgPath: 'images/food2.jpg'
+        imgPath: 'images/food2.jpg',
+        stockCount: 5
     },
 
     {
@@ -22,9 +22,57 @@ let foodsData = [
         title: "Hamburger",
         content: "완전식품 햄버거",
         price: 11000,
-        stockCount: 0,
-        imgPath: 'images/food3.jpg'
-        
+        imgPath: 'images/food3.jpg',
+        stockCount: 0
+    },
+
+    {
+        id: "fd004",
+        title: "팬케이크",
+        content: "메이플 시럽 잔뜩",
+        price: 8000,
+        imgPath: "images/food4.jpg",
+    },
+    {
+        id: "fd005",
+        title: "이름 모를 음식",
+        content: "보긴 많이 봤는데",
+        price: 13000,
+        imgPath: "images/food5.jpg",
+        stockCount: 3
+    },
+    {
+        id: "fd006",
+        title: "피자",
+        content: "1인에 한판 피자",
+        price: 15000,
+        imgPath: "images/food6.jpg",
+        stockCount: 5
+    },
+
+    {
+        id: "fd007",
+        title: "연어 스테이크",
+        content: "고양이도 탐내는",
+        price: 9000,
+        imgPath: "images/food7.jpg",
+        stockCount: 7
+    },
+    {
+        id: "fd008",
+        title: "새우구이",
+        content: "살짝 탄맛",
+        price: 11000,
+        imgPath: "images/food8.jpg",
+        stockCount: 9
+    },
+    {
+        id: "fd009",
+        title: "와플",
+        content: "크림이 없어요",
+        price: 2500,
+        imgPath: "images/food9.jpg",
+        stockCount: 1
     }
 ];
 
